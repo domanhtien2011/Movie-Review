46c3bc02ace0db72cb0b137424bda442874932bb
domanhtien2011 <domanhtien2011@gmail.com>
2015-12-16 09:52:09 +0700

    Covert review ratings to stars

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,14 @@ class MoviesController < ApplicationController
   # GET /movies/1
   # GET /movies/1.json
   def show
+    @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+
+    # Average review
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating).round(3)
+    end
   end
 
   # GET /movies/new
